305c417d4362c5084b217e519a3a6880d542998b
Алина Зарипова <khandyshka.92@mail.ru>
2022-04-30 23:16:47 +0300

    calculate

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int ticket = 30;
-        int bonus = ticket / 20 * 1;
-        System.out.println("Бонус:" + bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
